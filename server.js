ca097ae89784f01de37a9288da4971a818e57538
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-28 11:46:23 -0500

    api touch ups

--- server.js
+++ server.js
@@ -18,5 +18,24 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com:8000/api/videos');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 //Routing
 app.use('/api', require('./routes/api'));
